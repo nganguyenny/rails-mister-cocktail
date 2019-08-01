f12ecf7763511f7dd43ac99647800f4afb91eec6
Nga Nguyen <nganguyen.ny@gmail.com>
2019-08-01 15:31:41 +0800

    add controllers for cocktails, doses, ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_034051) do
+ActiveRecord::Schema.define(version: 2019_08_01_072826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,13 +33,10 @@ ActiveRecord::Schema.define(version: 2019_08_01_034051) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.bigint "cocktail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cocktail_id"], name: "index_ingredients_on_cocktail_id"
   end
 
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
-  add_foreign_key "ingredients", "cocktails"
 end
