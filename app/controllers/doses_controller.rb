b641743d9db556e1c4aa84fa89c4ebb48aecd212
Nga Nguyen <nganguyen.ny@gmail.com>
2019-08-02 14:02:48 +0800

    Add latest updates on gemfile and routes

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,7 +20,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@dose.cocktail)
+    redirect_to cocktail_path(@dose.cocktail.id)
   end
 
   private
